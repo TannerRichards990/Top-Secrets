1c35f977814e1a5596938862517c73a746f46fef
Tanner Richards <Tanner.richards990@gmail.com>
2022-11-16 17:25:14 -0800

    added cookie parser to app.js

--- sql/setup.sql
+++ sql/setup.sql
@@ -13,7 +13,7 @@ CREATE TABLE
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password_hash TEXT NOT NULL
   );
 
 CREATE TABLE
